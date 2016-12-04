4657553fb5556355ffe042296477f04fe9a0c345
amit kumar soni <amitsoniforbhu@gmail.com>
2016-12-04 11:55:18 +0530

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -1,28 +1,25 @@
-$(document).ready(function(){
-    $('#search').keyup(function(){
-		var s = $('#search').val();
-		
+$(document).ready(function(){    
+    $('#search_res').click(function(){
+    $('.well').remove(); //removes element when new search is initialized...
+		var s = $('#search').val(); // getting value from user....		
 		$.ajax({
 			url:"https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srlimit=8&callback=?&srsearch="+s,
-			//https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&UTF-8=&list=search&srlimit=8&callback=?&srsearch=hello
 			type:"GET",
 			dataType:"jsonp",
 			success: function(responseData){
 				
-				//alert(responseData.query.search.length);
+			//Displaying Obtained Data ... // 
 				for(var i=0;i<responseData.query.search.length ;i++){
 					$('.searsh-result').after(
 							'<div class="well"><h3>'+responseData.query.search[i].title+'</h3>'
 							+ responseData.query.search[i].snippet
 							+'<hr/><a href="https://en.wikipedia.org/wiki/' + responseData.query.search[i].title 
 							+'">View Full Details From Wiki</a></div>').addClass('anim',1000,'easeinOutBounce');	
-					 console.log(responseData.query.search[i].title);
-				}				
-				console.log(responseData);
-								
+					 
+				}											
 			},
 			error: function(){
-				alert('ERROR OCCURED ');		
+				alert('OOPS..! ERROR OCCURED WHILE SEARCHING DATA .... ! ');		
 			}
 			
 		});
